2ef9f12b9f51e51563b7098af6f2d44d18372e96
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-03-14 14:52:49 +0100

    first part of websocket rewrite

---
+++ resources/js/websockethandler.js
@@ -0,0 +1,28 @@
+var wsHandler = {
+    socket: null,
+    connections: [],
+
+    init: function() {
+        wsHandler.socket = io('https://socket.assembl.science:8080');
+        wsHandler.socket.on('connect', function() {
+            console.log("Websocket connected");
+        });
+        wsHandler.socket.on('disconnect', function() {
+            console.warn("Websocket disconnected");
+        });
+        wsHandler.socket.on('reconnect_attempt', function(attemptNumber) {
+            console.log("Attempting to reconnect to websocket ("+attemptNumber+")...");
+        });
+        wsHandler.socket.on('reconnect', function() {
+            console.log("Reconnected to the websocket");
+        });
+        wsHandler.socket.on('error', function(err) {
+            console.error(err);
+        });
+        wsHandler.socket.on('connect_error', function(timeout) {
+            console.warn("Websocket connection attempt timed out");
+        });
+    }
+};
+
+wsHandler.init();
