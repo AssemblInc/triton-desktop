64900c7d146d310fd4a0b64c1f4e0d378c4f1883
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-05-03 20:43:38 +0200

    rewrote stellar handler

--- resources/js/stellarhandler.js
+++ resources/js/stellarhandler.js
@@ -1,9 +1,51 @@
 const StellarSdk = require('stellar-sdk');
-let horizonServer = new StellarSdk.Server('https://horizon.stellar.org');
+
+let stellarHandler = {
+    initialized: false,
+    horizonServer: new StellarSdk.Server('https://horizon.stellar.org'),
+    keypair: null,
+    account: null,
+
+    init: function() {
+        if (!stellarHandler.initialized) {
+            StellarSdk.Network.usePublicNetwork();
+            stellarHandler.keypair = StellarSdk.Keypair.fromSecret("***REMOVED_PRIV_KEY_STELLAR_HORIZON_SOURCE_ACC***");
+            stellarHandler.account = await stellarHandler.horizonServer.loadAccount(stellarHandler.keypair.publicKey());
+        }
+        else {
+            console.warn("stellarHandler was already initialized!");
+        }
+    },
+
+    commitHash: function(hash) {
+        if (hash == null) {
+            console.warn("Hash equals null! Not commiting hash");
+            return;
+        }
+
+        if (stellarHandler.account == null) {
+            console.warn("stellarHandler.account equals null! Not commiting hash");
+            return;
+        }
+
+        const transaction = new StellarSdk.TransactionBuilder(stellarHandler.account)
+            .setTimeout(0)
+            .addOperation(StellarSdk.Operation.bumpSequence({
+                bumpTo: stellarHandler.account.sequenceNumber() + 1
+            }))
+            .addMemo(new StellarSdk.Memo(StellarSdk.MemoHash, hash))
+            .build();
+        transaction.sign(stellarHandler.keypair);
+        return stellarHandler.horizonServer.submitTransaction(transaction);
+    }
+};
+
+stellarHandler.init();
+
 /*
-let keypair = StellarSdk.Keypair.random();
-*/
-let keypair = StellarSdk.Keypair.fromSecret("***REMOVED_PRIV_KEY_STELLAR_HORIZON_SOURCE_ACC***");
+let horizonServer = ;
+// let keypair = StellarSdk.Keypair.random();
+let keypair = ;
 console.log(keypair);
 console.log("Public key:", keypair.publicKey());
 console.log("Secret key:", keypair.secret());
@@ -70,3 +112,4 @@ async function create() {
 };
 
 // create();
+*/
