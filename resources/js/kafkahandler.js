0b81aa4b891a4c7fea945faad6e3f658977432ea
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-05-13 15:39:57 +0200

    added test kafka code

---
+++ resources/js/kafkahandler.js
@@ -0,0 +1,51 @@
+const { Kafka } = require('kafkajs');
+
+let kafkaHandler = {
+    kafka: null,
+    producer: null,
+    consumer: null,
+
+    init: function() {
+        kafkaHandler.kafka = new Kafka({
+            clientId: 'science.assembl.desktop',
+            brokers: ['localhost:9092']
+        });
+    },
+
+    setUpProducer: async function() {
+        kafkaHandler.producer = kafkaHandler.kafka.producer();
+        await kafkaHandler.producer.connect();
+        await kafkaHandler.producer.send({
+            topic: 'test-topic',
+            messages: [
+                {
+                    value: 'this is a test!'
+                }
+            ]
+        });
+        await kafkaHandler.producer.disconnect();
+    },
+
+    setUpConsumer: async function() {
+        kafkaHandler.consumer = kafkaHandler.kafka.consumer({
+            groupId: 'test-group'
+        });
+        await kafkaHandler.consumer.connect();
+        await kafkaHandler.consumer.subscribe({
+            topic: 'test-topic'
+        });
+        await kafkaHandler.consumer.run({
+            eachMessage: async function({ topic, partition, message }) {
+                console.log({
+                    value: message.value.toString()
+                });
+            }
+        });
+    }
+};
+
+/*
+kafkaHandler.init();
+kafkaHandler.setUpProducer();
+kafkaHandler.setUpConsumer();
+*/
