a4f48e85df1b903795fd13efb7d2a55eb26f62e8
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-05-14 10:43:27 +0200

    combined appendinitvect with userdatahandler

--- resources/js_modules/appendinitvect.js
+++ resources/js_modules/appendinitvect.js
@@ -1,22 +1,3 @@
-/* from https://gist.githubusercontent.com/bbstilson/e2617cf1375481a34c0b6d9ecf0879bf/raw/3f16210ec2ee04c3acd8d92b36dd00f7d170f619/appendInitVect.js */
 
-const { Transform } = require('stream');
-
-class AppendInitVect extends Transform {
-  constructor(initVect, opts) {
-    super(opts);
-    this.initVect = initVect;
-    this.appended = false;
-  }
-
-  _transform(chunk, encoding, cb) {
-    if (!this.appended) {
-      this.push(this.initVect);
-      this.appended = true;
-    }
-    this.push(chunk);
-    cb();
-  }
-}
 
 module.exports = AppendInitVect;
--- resources/js_modules/userdatahandler.js
+++ resources/js_modules/userdatahandler.js
@@ -7,7 +7,26 @@ const path = require('path');
 const zlib = require('zlib');
 const stream = require('stream');
 const keccak256 = require('js-sha3').keccak256;
-const AppendInitVect = require('./appendInitVect');
+
+/* from https://gist.githubusercontent.com/bbstilson/e2617cf1375481a34c0b6d9ecf0879bf/raw/3f16210ec2ee04c3acd8d92b36dd00f7d170f619/appendInitVect.js */
+const { Transform } = require('stream');
+class AppendInitVect extends Transform {
+  constructor(initVect, opts) {
+    super(opts);
+    this.initVect = initVect;
+    this.appended = false;
+  }
+
+  _transform(chunk, encoding, cb) {
+    if (!this.appended) {
+      this.push(this.initVect);
+      this.appended = true;
+    }
+    this.push(chunk);
+    cb();
+  }
+};
+/* end from */
 
 const file = path.join(app.getPath('userData'), 'userdata.assemblsec');
 const prevSessionExists = fs.existsSync(file);
