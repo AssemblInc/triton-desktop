933d2b856d703721d025fb692d87671d636b2cfa
Freek Bes <36384333+FreekBes@users.noreply.github.com>
2019-05-17 13:46:12 +0200

    wrote first hopefully working version of httphandler

--- resources/js/httphandler.js
+++ resources/js/httphandler.js
@@ -5,33 +5,91 @@ let qs = require('querystring');
 
 let httpHandler = {
     server: null,
+    requiredAuth: Math.random().toString(24).substring(2)+":"+Math.random().toString(36).substring(2),
+    sendTo: {
+        initialized: false,
+        url: null,
+        auth: null
+    },
 
     startServer: function() {
         server = http.createServer(function(req, res) {
-            if (req.method == 'POST') {
-                let chunks = [];
-                req.on('data', function(chunk) {
-                    chunks.push(chunk);
-                    // dont forget to implement stream-meter instead to fix memory attacks
-                    /*
-                    limitedStream = request.pipe(meter(1e7));
-                    limitedStream.on('data', ...);
-                    limitedStream.on('end', ...);
-                    */
-                });
-                req.on('end', function() {
-                    let data = Buffer.concat(chunks);
-                });
+            let header = req.headers['authorization'] || '';
+            let token = header.split(/\s+/).pop() || '';
+            let auth = new Buffer.from(token, 'base64').toString();
+
+            if (auth === httpHandler.requiredAuth) {
+                if (req.method == 'POST') {
+                    let chunks = [];
+                    req.on('data', function(chunk) {
+                        chunks.push(chunk);
+                    });
+                    req.on('end', function() {
+                        let data = Buffer.concat(chunks);
+                        res.writeHead(200, {'Content-Type':'application/json'});
+                        res.end(JSON.stringify({
+                            'chunkNumber': 0,
+                            'received': true
+                        }));
+                        console.log("Received data over HTTP: ", data);
+                    });
+                }
+                else {
+                    // method incorrect, should always be POST
+                    res.writeHead(405, {'Content-Type':'application/json'});
+                    res.end(JSON.stringify({
+                        'error': 'method_not_allowed',
+                        'error_msg': 'only POST is supported by this server'
+                    }));
+                    console.warn("Someone tried to connect over HTTP not using POST method!");
+                }
             }
             else {
-                response.writeHead(405, {'Content-Type': 'text/plain'});
-                response.end();
+                // password or username incorrect
+                res.writeHead(401, {'Content-Type':'application/json'});
+                res.end(JSON.stringify({
+                    'error': 'unauthorized',
+                    'error_msg': 'wrong or no authorization credentials provided'
+                }));
+                console.warn("Someone tried to connect over HTTP with the wrong or no credentials!");
             }
         }).listen(27626);
     },
 
     stopServer: function() {
         server.close();
+    },
+
+    initSender: function(url, authorization) {
+        httpHandler.sendTo.initialized = true;
+        httpHandler.sendTo.url = url;
+        httpHandler.sendTo.auth = authorization;
+    },
+
+    sendChunk: function(chunk, isEncrypted, number) {
+        if (httpHandler.sendTo.initialized) {
+            if (isEncrypted) {
+                let xhr = new XMLHttpRequest();
+                xhr.onreadystatechange = function() {
+                    if (this.readyState == 4) {
+                        let response = JSON.parse(this.responseText);
+                        console.log("HTTP response: ", response);
+                    }
+                };
+                let params = 'encrypted=1&number='+parseInt(number)+'&chunk='+chunk;
+                xhr.open('POST', httpHandler.sendTo.url, true);
+                xhr.setRequestHeader("Authorization", "Basic "+btoa(httpHandler.sendTo.auth));
+                xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+                xhr.setRequestHeader("Accept", "application/json")
+                xhr.send(params);
+            }
+            else {
+                ipcRenderer.send('pgp-encrypt-chunk', chunk, number);
+            }
+        }
+        else {
+            console.warn("Tried sending data over HTTP while the connection details were not initialized yet.");
+        }
     }
 };
 
